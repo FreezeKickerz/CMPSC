82eef7b436c41561e8f3033a1fc1b4516691e9d0
Tyler Laptop <FreezeKickerz@icloud.com>
2023-03-04 03:56:47 -0500

    3:56 AM

--- CMPSC_PROJECTS/C_C++/test.c
+++ CMPSC_PROJECTS/C_C++/test.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 
 int main(){
-    printf("Hello Worldss");
+    char characterName[] = "John";
+    int characterAge = 35;
+    printf("There once was a man named %s.\n" , characterName);
+    printf("He was %d years old.\n", characterAge);
+    printf("He really liked the name %s.\n", characterName);
+    printf("But he did not like being %d.\n", characterAge);
     return 0;
 }
