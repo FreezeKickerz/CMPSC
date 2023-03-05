7fec1bafa259bff1bb61bf0f52a613de1b30e3d1
Tyler Laptop <FreezeKickerz@icloud.com>
2023-03-05 17:37:10 -0500

    5:37PM

---
+++ CMPSC_PROJECTS/C_C++/math.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void main(){
+    const int num = 65;
+    int age;
+
+    printf("%d\n", num);
+    printf("%d\n", num);
+    printf("%f\n", floor(36.356));
+
+    printf("Enter you age: \n");
+    scanf("%d", &age);
+    printf("You age is %c!", age);
+}
