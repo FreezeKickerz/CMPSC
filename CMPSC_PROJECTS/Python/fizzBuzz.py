391d9627e37520b53bfda32c18941f05cfd6b031
Tyler Laptop <FreezeKickerz@icloud.com>
2023-03-05 18:31:23 -0500

    3/5/23 6:31PM

---
+++ CMPSC_PROJECTS/Python/fizzBuzz.py
@@ -0,0 +1,15 @@
+n = int(input("Enter a number greater than 0: "))
+def fizzBuzz(n: int):
+    answer = []
+    while n != 0:
+        if n % 5 == 0 and n % 3 == 0:
+            answer.insert(0, "FizzBuzz")
+        elif n % 3 == 0:
+            answer.insert(0, "Fizz")
+        elif n % 5 == 0:
+            answer.insert(0, "Buzz")
+        else:
+            answer.insert(0, str(n))
+        n -= 1
+    return answer
+print(fizzBuzz(n))
